863fde5edea29e2a0af3f2225af3fbf11e9600f2
Osman Perviz <osman.perviz@evopark.de>
2017-02-19 21:50:16 +0100

    Groupe middleware

---
+++ middleware/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import bodyParser from 'koa-bodyparser';
+import cors from 'kcors';
+import logger from 'koa-logger'
+import compose from 'koa-compose';
+import convert from 'koa-convert';
+
+export default function () {
+  return compose([
+    // cors(),
+    // bodyParser(),
+     logger()
+  ])
+}
--- src/app.js
+++ src/app.js
@@ -1,18 +1,12 @@
 import Koa from 'koa';
 import config from './config';
-import bodyParser from 'koa-bodyparser';
-import cors from 'kcors';
-import logger from 'koa-logger'
-import middleware from 'koa-router';
-const router = middleware();
-
+import middleware from '../middleware'
 
 const app = new Koa()
-  .use(cors())
-  .use(bodyParser())
-  .use(logger())
+  .use(middleware())
   .use(async (ctx) => {
-     ctx.body = 'Hello!!!s'
+    console.log(ctx)
+     ctx.body = 'Hello!!!'
 });
 
 export default app
